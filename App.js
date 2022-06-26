2a24aef5ed3128cb65a80092b9d39b2e3ef101bc
Minhazur Rahman <rafi.rahman03@yahoo.com>
2022-06-26 23:21:35 +0600

    project created and dependencies added

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
+import Categories from "./app/screens/Categories";
+import MealsOverview from "./app/screens/MealsOverview";
+import MealDetails from "./app/screens/MealDetails";
+
+const Stack = createNativeStackNavigator();
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    return (
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+        <NavigationContainer>
+            <StatusBar style={"light"}/>
+            <Stack.Navigator screenOptions={{
+                headerStyle: {backgroundColor: "#351401"},
+                headerTintColor: "white",
+                contentStyle: {backgroundColor: "#3f2f25"}
+            }}
+            >
+                <Stack.Screen
+                    name="MealCategories"
+                    component={Categories}
+                    options={{title: "All Categories"}}/>
+                <Stack.Screen
+                    name="MealOverview"
+                    component={MealsOverview}
+                />
+                <Stack.Screen
+                    name="MealDetails"
+                    component={MealDetails}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+};
