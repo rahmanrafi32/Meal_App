f33fea57a6e9cbce5c27aa88c7a8663ad6d18d35
Minhazur Rahman <rafi.rahman03@yahoo.com>
2022-06-27 00:22:20 +0600

    meal detail created

---
+++ app/components/MealDetail.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+    details: {
+        flexDirection: "row",
+        justifyContent: "center",
+        padding: 8,
+    },
+    detailItem: {
+        marginHorizontal: 4,
+        fontSize: 14,
+    }
+});
+
+const MealDetail = ({duration, complexity, affordability, style, textStyle}) => {
+    return (
+        <View style={[styles.details, style]}>
+            <Text style={[styles.detailItem, textStyle]}>Time: {duration}Min</Text>
+            <Text style={[styles.detailItem, textStyle]}>Procedure: {complexity.toUpperCase()}</Text>
+        </View>
+    );
+};
+
+export default MealDetail;
